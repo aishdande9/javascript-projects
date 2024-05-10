28eb502b6eee128a25e0ac2ee3af489d8ed213ee
aish.dande9 <aish.dande@gmail.com>
2024-05-09 21:50:04 -0500

    studio array

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -5,44 +5,70 @@ let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
+
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	let check;
-	let output;
-	//TODO: 2. write the code required for this step
+	let str = "up,to,code,fun";
 
+	let str1  = str.split(",");
+
+	let check = str1.reverse();
+	let output = check.join(",");
+	//TODO: 2. write the code required for this step
+console.log(output);
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
+reverseCommas()
 
 //3)
 function semiDash() {
-	let check;
-	let output;
+	let str1 = "up;to;code;fun"
+
+    let str2 = str1.split(";");
+    console.log(str2);
+
+    let check = str2.sort();
+    console.log(check);
+
+    let output = check.join("-");
+    console.log(output);
+
 //TODO: write the code required for this step
 
   
 	return output;
 }
+semiDash()
 
 //4)
 function reverseSpaces() {
-	let check;
-	let output;
+	    let str = "to code up fun";
+    let check = str.split(" ");
+    console.log(check);
+
+    let checks = check.sort();
+    console.log(checks);
+
+    let output = checks.join(" ");
+    console.log(output);
   //TODO: write the code required for this step
 
 	return output;
 }
+reverseSpaces()
 
 //5)
 function commaSpace() {
-	let check;
-	let output;
+	let str = "up, to, code, fun";
+	let check = str.split(", ");
+	let output= check.join(",");
 	//TODO: write the code required for this step
-  
+  console.log(output);
 	return output;
 }
+commaSpace()
 
 // NOTE: Don't add or modify any code below this line or your program might not run as expected.
 module.exports = {
--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -5,10 +5,53 @@ let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
 
+let cabinet1 = food.split(",")
+console.log(cabinet1);
+let cabinet2 = equipment.split(",")
+console.log(cabinet2)
+
+let cabinet3 = pets.split(",")
+console.log(cabinet3)
+
+let cabinet4 = sleepAids.split(",")
+console.log(cabinet4)
+
+
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+
+let cargoHold = [];
+cargoHold.push(cabinet1);
+cargoHold.push(cabinet2);
+cargoHold.push(cabinet3);
+cargoHold.push(cabinet4);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
 
+const input = require("readline-sync");
+
+userInput = input.question("Select a cabinet between (0-3)in the cargoHold :  ");
+console.log(`User selected cabinet: ${userInput}`);
+
+
+
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
 
+if(userInput>cargoHold.length){
+    console.log("Error!!!! you entered an invalid number")
+}
+else {
+    console.log(cargoHold[userInput]);
+}
+
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+cargoNo = input.question("Select a cabinet between (0-3)in the cargoHold :  ");
+console.log(`User selected cabinet: ${cargoNo}`);
+item = input.question("please enter the item : ");
+console.log(`User selected item: ${item}`);
+
+if(cargoHold[cargoNo].includes(item)){
+    console.log(`Cabinet ${cargoNo} DOES contain ${item}.`);
+}
+else{
+    console.log(`Cabinet ${cargoNo} DOES NOT contain ${item}.`);
+}
