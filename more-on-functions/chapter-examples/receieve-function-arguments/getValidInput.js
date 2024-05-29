a52c2853ee63d48357f1e57f903cfe67a6e14fa7
aish.dande9 <aish.dande@gmail.com>
2024-05-29 16:16:31 -0500

    Assignment exercises!

--- more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
+++ more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
@@ -14,8 +14,28 @@ function getValidInput(prompt, isValid) {
 
 // TODO 1: write a validator 
 // that ensures input starts with "a"
+let isLetter = function(strValue){
+  if(strValue.startsWith('a')){
+    return true;
+  }else{
+    return false;
+  }
+};
+
+console.log(getValidInput("Enter the string value :",isLetter));
 
 // TODO 2: write a validator 
 // that ensures input is a vowel
+let isVowel = function(str1){
+  let vowels = ['a','e','i','o','u'];
+  if(vowels.includes(str1))
+  {
+    return true;}
+    else{
+      return false;
+    }
+  };
+  
+  console.log(getValidInput("Enter the input that is vowel :",isVowel));
 
 // Be sure to test your code!
