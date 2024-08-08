39c53f59b6ac81911b5b0aa75665a730de0cc805
aish.dande9 <aish.dande@gmail.com>
2024-08-07 22:41:19 -0500

    exercises

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,24 @@
 //Code Your Solution Below
+window.addEventListener("load", () => {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    let testname = document.querySelector("input[name = testname]");
+    let testdate = document.querySelector("input[name = flightdate]");
+
+    let windrating = document.querySelector("input[name=windRating]");
+    let rocketboosters = document.querySelector("input[name=boosterCount]");
+    let productiongrade = document.querySelector(
+      "input[name=productionServers]"
+    );
+    if (
+      testname.value === "" ||
+      testdate.value === "" ||
+      windrating.value === "" ||
+      rocketboosters.value === "" ||
+      productiongrade.value === ""
+    ) {
+      alert("All fields are required!");
+      event.preventDefault();
+    }
+  });
+});
